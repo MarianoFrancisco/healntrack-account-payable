840a76391e28e58e3dae5b34246d7258b5cff0d7
LuisCif-20 <n.rodas225@gmail.com>
2025-10-29 00:04:11 -0600

    chore: add project structure

---
+++ account-payable-service/src/main/java/com/sa/healntrack/account_payable_service/AccountPayableServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sa.healntrack.account_payable_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountPayableServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountPayableServiceApplication.class, args);
+	}
+
+}
---
+++ account-payable-service/src/test/java/com/sa/healntrack/account_payable_service/AccountPayableServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sa.healntrack.account_payable_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountPayableServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
